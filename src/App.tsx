124a282853e27314354a7e4d26efd803270071c5
Florjan96 <florianshiqerukaj@yahoo.com>
2023-09-06 13:40:48 +0200

    Created state also created a function for flip coin with Math.random()

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,23 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [face, setFace] = useState('Kok')
+
+
+  function flipCoin(){
+    const newFlipCoin= Math.random() >0.5 ?"Kok": "Pil"
+    setFace(newFlipCoin)
+
+  }
+
+
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+
+      <h1>{face}</h1>
+      <button onClick={flipCoin}>Rrotulloje</button>
+      
     </div>
   )
 }
